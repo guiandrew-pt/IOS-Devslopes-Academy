5a4375d19965a90093fee68ff40539b50b96f6b0
GuiAndrew <guiandrew.pt@outlook.com>
2021-09-19 22:24:55 +0100

    Fix WindowShopper app

---
+++ Week 4 - UIKit/03 - App - WindowShopper /window-shopper/window-shopper/Controller/MainVC.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Guilherme Andre on 23/08/2021.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    // Outlet:
+    @IBOutlet weak var wageTxt: CurrencyTxtField!
+    @IBOutlet weak var priceTxt: CurrencyTxtField!
+    @IBOutlet weak var resultLbl: UILabel!
+    @IBOutlet weak var hoursLbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 0.9952759147, green: 0.5924168229, blue: 0.02002472617, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+        
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
+    }
+
+    @objc func calculate() {
+        if ((wageTxt.text?.contains(",")) != nil) {
+            wageTxt.text = wageTxt.text?.replacingOccurrences(of: ",", with: ".")
+        }
+        
+        if ((priceTxt.text?.contains(",")) != nil) {
+            priceTxt.text = priceTxt.text?.replacingOccurrences(of: ",", with: ".")
+        }
+        
+        if let wageTxt = wageTxt.text, let priceTxt = priceTxt.text {
+            if let wage = Double(wageTxt), let price = Double(priceTxt) {
+                view.endEditing(true)
+                resultLbl.isHidden = false
+                hoursLbl.isHidden = false
+                resultLbl.text = "\(Wage.getHours(forWage: wage, andPrice: price))"
+            }
+        }
+    }
+    
+    @IBAction func clearCalculatorPressed(_ sender: Any) {
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
+        wageTxt.text = ""
+        priceTxt.text = ""
+    }
+    
+    
+}
+
---
+++ Week 4 - UIKit/03 - App - WindowShopper /window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,54 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by Guilherme Andre on 23/08/2021.
+//
+
+import UIKit
+
+@IBDesignable
+class CurrencyTxtField: UITextField {
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        
+        // X -> represent the left;
+        // Y -> represents the top;
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.882735014, green: 0.8774878383, blue: 0.8867686987, alpha: 0.8)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        customizeView()
+    }
+
+    func customizeView() {
+        backgroundColor = #colorLiteral(red: 0.9961728454, green: 0.9902502894, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        clipsToBounds = true
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+}
